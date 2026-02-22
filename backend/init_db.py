998cd1ab09eb795d5228cd0f3f4f167a3729d3af
Rosa María Espinosa Martínez <rosamaria.espinosamartinez16@gmail.com>
2026-02-22 11:59:46 +0100

    remove: delete init_db.py script in favor of Alembic migrations

--- backend/init_db.py
+++
@@ -1,7 +0,0 @@
-from database import Base, engine
-from models import Usuario, Itinerario, Noticia, Emisora, Hermandad, InfoPaso
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-
-print("Tablas eliminadas y creadas correctamente.")
