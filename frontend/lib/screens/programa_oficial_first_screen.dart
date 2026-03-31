56e9bec76271971adfae7541cdea9523ff4238f1
Rosa María Espinosa Martínez <rosamaria.espinosamartinez16@gmail.com>
2026-03-31 15:12:24 +0200

    feat: implement ProgramaFirstScreen with dynamic day loading and navigation

---
+++ frontend/lib/screens/programa_oficial_first_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+import 'programa_oficial_second_screen.dart';
+
+class ProgramaFirstScreen extends StatefulWidget {
+  const ProgramaFirstScreen({super.key});
+
+  @override
+  State<ProgramaFirstScreen> createState() => _ProgramaFirstScreenState();
+}
+
+class _ProgramaFirstScreenState extends State<ProgramaFirstScreen> {
+  late List<String> listaDias = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _cargarDias();
+  }
+
+  Future<void> _cargarDias() async {
+    final dias = await Dia.getDiasSemanaSanta();
+    setState(() {
+      listaDias = dias.map((dia) => (dia.nombre)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: listaDias.length,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                final nombre = listaDias[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProgramaSecondScreen(idDia: index + 1, nombreDia: nombre),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+                      backgroundColor: const Color.fromARGB(255, 26, 19, 92),
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      nombre,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
