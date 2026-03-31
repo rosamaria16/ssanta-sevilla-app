ecde4e4354ee405610f61c00620eb08bfce3be07
Rosa María Espinosa Martínez <rosamaria.espinosamartinez16@gmail.com>
2026-03-31 15:11:29 +0200

    feat: add Dia class with needed methods for the Programa Oficial functionality

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -76,3 +76,81 @@ class ApiService {
     AuthManager().logout();
   }
 }
+
+// Dia (para poder sacar los datos de la BD)
+class Dia {
+  final int id;
+  final String nombre;
+
+  Dia({
+    required this.id,
+    required this.nombre
+  });
+
+  factory Dia.fromJson(Map<String, dynamic> json) {
+    return Dia(
+      id: json['id'],
+      nombre: json['nombre']
+    );
+  }
+
+  static Future<List<Dia>> getDiasSemanaSanta() async {
+    try {
+      final response = await http.get(
+        Uri.parse('$apiBaseUrl/dias/'),
+      ).timeout(requestTimeout, onTimeout: () {
+        throw Exception('Tiempo de conexión agotado');
+      });
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List<dynamic>;
+        return data.map((dia) => Dia.fromJson(dia)).toList();
+      } else if (response.statusCode >= 500) {
+        throw Exception('Error en el servidor');
+      } else {
+        throw Exception('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception(e.toString().replaceFirst('Exception: ', ''));
+    }
+  }
+}
+
+// Dia (para poder sacar los datos de la BD)
+class Hermandad {
+  final int id;
+  final String nombre;
+
+  Hermandad({
+    required this.id,
+    required this.nombre
+  });
+
+  factory Hermandad.fromJson(Map<String, dynamic> json) {
+    return Hermandad(
+      id: json['id'],
+      nombre: json['nombre']
+    );
+  }
+
+  static Future<List<Hermandad>> getHermandadesDia(int idDia) async {
+    try {
+      final response = await http.get(
+        Uri.parse('$apiBaseUrl/hermandades/dia/$idDia'),
+      ).timeout(requestTimeout, onTimeout: () {
+        throw Exception('Tiempo de conexión agotado');
+      });
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List<dynamic>;
+        return data.map((hdad) => Hermandad.fromJson(hdad)).toList();
+      } else if (response.statusCode >= 500) {
+        throw Exception('Error en el servidor');
+      } else {
+        throw Exception('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception(e.toString().replaceFirst('Exception: ', ''));
+    }
+  }
+}
